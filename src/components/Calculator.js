eb5f8e5e6d95dee8b47c4172e5bb60d8147c6da8
Christabelle143 <christabelleyouta143@gmail.com>
2022-07-08 02:41:38 +0100

    fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,55 +1,47 @@
 import React from 'react';
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
-//   constructor(props) {
-//     super(props);
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-//     this.state = {
-//       total: 0,
-//       next: 0,
-//     };
-
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick(e) {
-//     const operation = e.target.value;
-//     const result = calculate(this.state, operation);
-//     this.setState(result);
-//   }
+  handleClick(event) {
+    this.setState((state) => calculate(state, event.target.textContent));
+  }
 
   render() {
-    // const { next, total } = this.state;
+    const { next, total } = this.state;
     return (
       <>
-        <div className="calculator">
-          <div className="calc-input"><span className="output">{next || total || 0}</span></div>
-          <div className="calc-buttons">
-          <button type="button" className="button1">AC</button>
-            <button type="button" className="button1">+/-</button>
-            <button type="button" className="button1">%</button>
-            <button type="button" className="button2 active"><span>&#247;</span></button>
-            <button type="button" className="button1">7</button>
-            <button type="button" className="button1">8</button>
-            <button type="button" className="button1">9</button>
-            <button type="button" className="button2 active">x</button>
-            <button type="button" className="button1">4</button>
-            <button type="button" className="button1">5</button>
-            <button type="button" className="button1">6</button>
-            <button type="button" className="button2 active">-</button>
-            <button type="button" className="button1">1</button>
-            <button type="button" className="nbutton1">2</button>
-            <button type="button" className="button1">3</button>
-            <button type="button" className="button2 active">+</button>
-            <button type="button" className="button1">0</button>
-            <button type="button" className="button1">.</button>
-            <button type="button" className="button2 active">=</button>
+        <div className="calculator-container">
+          <div className="calc-input"><span className="result">{next || total || 0}</span></div>
+          <div className="buttons">
+            <button type="button" className="bg-1" onClick={this.handleClick}>AC</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>+/-</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>%</button>
+            <button type="button" className="bg-1 active" onClick={this.handleClick}><span>&#247;</span></button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>7</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>8</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>9</button>
+            <button type="button" className="bg-1 active" onClick={this.handleClick}>x</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>4</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>5</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>6</button>
+            <button type="button" className="bg-1 active" onClick={this.handleClick}>-</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>1</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>2</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>3</button>
+            <button type="button" className="bg-1 active" onClick={this.handleClick}>+</button>
+            <button type="button" className="zero" onClick={this.handleClick}>0</button>
+            <button type="button" className="bg-1" onClick={this.handleClick}>.</button>
+            <button type="button" className="bg-1 active" onClick={this.handleClick}>=</button>
           </div>
         </div>
       </>
     );
   }
 }
-
 export default Calculator;
