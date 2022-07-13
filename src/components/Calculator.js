aab8c689ec0251f52cdb502449afaa8d08043378
Christabelle143 <christabelleyouta143@gmail.com>
2022-07-13 21:21:59 +0100

    use routers for spa

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,28 +8,33 @@ const Calculator = () => {
   };
   return (
     <>
-      <div className="calculator-container">
-        <div className="calc-input"><span className="result">{state.next || state.total || 0}</span></div>
-        <div className="buttons">
-          <button type="button" className="bg-1" onClick={handleClick}>AC</button>
-          <button type="button" className="bg-1" onClick={handleClick}>+/-</button>
-          <button type="button" className="bg-1" onClick={handleClick}>%</button>
-          <button type="button" className="bg-1 active" onClick={handleClick}><span>&#247;</span></button>
-          <button type="button" className="bg-1" onClick={handleClick}>7</button>
-          <button type="button" className="bg-1" onClick={handleClick}>8</button>
-          <button type="button" className="bg-1" onClick={handleClick}>9</button>
-          <button type="button" className="bg-1 active" onClick={handleClick}>x</button>
-          <button type="button" className="bg-1" onClick={handleClick}>4</button>
-          <button type="button" className="bg-1" onClick={handleClick}>5</button>
-          <button type="button" className="bg-1" onClick={handleClick}>6</button>
-          <button type="button" className="bg-1 active" onClick={handleClick}>-</button>
-          <button type="button" className="bg-1" onClick={handleClick}>1</button>
-          <button type="button" className="bg-1" onClick={handleClick}>2</button>
-          <button type="button" className="bg-1" onClick={handleClick}>3</button>
-          <button type="button" className="bg-1 active" onClick={handleClick}>+</button>
-          <button type="button" className="zero" onClick={handleClick}>0</button>
-          <button type="button" className="bg-1" onClick={handleClick}>.</button>
-          <button type="button" className="bg-1 active" onClick={handleClick}>=</button>
+      <div className="maths">
+        <div className="math">
+          <h2>let`s do some math!</h2>
+        </div>
+        <div className="calculator-container">
+          <div className="calc-input"><span className="result">{state.next || state.total || 0}</span></div>
+          <div className="buttons">
+            <button type="button" className="bg-1" onClick={handleClick}>AC</button>
+            <button type="button" className="bg-1" onClick={handleClick}>+/-</button>
+            <button type="button" className="bg-1" onClick={handleClick}>%</button>
+            <button type="button" className="bg-1 active" onClick={handleClick}><span>&#247;</span></button>
+            <button type="button" className="bg-1" onClick={handleClick}>7</button>
+            <button type="button" className="bg-1" onClick={handleClick}>8</button>
+            <button type="button" className="bg-1" onClick={handleClick}>9</button>
+            <button type="button" className="bg-1 active" onClick={handleClick}>x</button>
+            <button type="button" className="bg-1" onClick={handleClick}>4</button>
+            <button type="button" className="bg-1" onClick={handleClick}>5</button>
+            <button type="button" className="bg-1" onClick={handleClick}>6</button>
+            <button type="button" className="bg-1 active" onClick={handleClick}>-</button>
+            <button type="button" className="bg-1" onClick={handleClick}>1</button>
+            <button type="button" className="bg-1" onClick={handleClick}>2</button>
+            <button type="button" className="bg-1" onClick={handleClick}>3</button>
+            <button type="button" className="bg-1 active" onClick={handleClick}>+</button>
+            <button type="button" className="zero" onClick={handleClick}>0</button>
+            <button type="button" className="bg-1" onClick={handleClick}>.</button>
+            <button type="button" className="bg-1 active" onClick={handleClick}>=</button>
+          </div>
         </div>
       </div>
     </>
